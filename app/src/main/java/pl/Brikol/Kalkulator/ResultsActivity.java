74f0a9fbe66457936834c02a7b70b243d183d192
GrandiPL <jakub.wachowiak@gmail.com>
2014-02-20 00:47:03 +0100

    Added second activity bundle event

--- app/src/main/java/pl/Brikol/Kalkulator/ResultsActivity.java
+++ app/src/main/java/pl/Brikol/Kalkulator/ResultsActivity.java
@@ -8,12 +8,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ResultsActivity extends Activity {
 
+    TextView test;
+    EditText test2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        test = (TextView) findViewById(R.id.textView2);
+        Bundle dane = getIntent().getExtras();
+        String[] mystrings = dane.getStringArray("dane");
+        test.setText(String.valueOf(Integer.parseInt(mystrings[2])*2));
     }
 }

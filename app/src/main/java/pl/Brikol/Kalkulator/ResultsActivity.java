8dacf8edc0c2b39c27156c2d28300941d70b701f
GrandiPL <jakub.wachowiak@gmail.com>
2014-02-20 01:34:32 +0100

    Added data handling

--- app/src/main/java/pl/Brikol/Kalkulator/ResultsActivity.java
+++ app/src/main/java/pl/Brikol/Kalkulator/ResultsActivity.java
@@ -11,18 +11,52 @@ import android.os.Build;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+
 public class ResultsActivity extends Activity {
 
-    TextView test;
+    TextView product, model;
     EditText test2;
+    float productivity, powerCons, parts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-        test = (TextView) findViewById(R.id.textView2);
+        product = (TextView) findViewById(R.id.textView);
+        model = (TextView) findViewById(R.id.textView2);
+
         Bundle dane = getIntent().getExtras();
-        String[] mystrings = dane.getStringArray("dane");
-        test.setText(String.valueOf(Integer.parseInt(mystrings[2])*2));
+        String[] data = dane.getStringArray("dane");
+        Init(data[0]);
+        model.setText(data[0]);
+        //Double z = new BigDecimal(1000/productivity).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        //test2.setText(z.toString());
+    }
+
+    public void Init(String data){
+        double power = 0.86;
+        switch(data){
+            case "ZSJ25":
+                productivity = 200;
+                powerCons = 17*(float)power;
+                break;
+            case "ZSJ28":
+                productivity = 220;
+                powerCons = 24*(float)power;
+                break;
+            case "ZSJ30":
+                productivity = 350;
+                powerCons = 32*(float)power;
+                break;
+            case "ZSJ50":
+                productivity = 400;
+                powerCons = 34*(float)power;
+                break;
+            case "ZSJ70":
+                productivity = 700;
+                powerCons = 65*(float)power;
+                break;
+        }
     }
 }
